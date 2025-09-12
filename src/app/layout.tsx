4513b47e360cabe3b2eaab37c829d6887331d574
Juan <juan_dlp2@outlook.com>
2025-09-12 17:06:44 -0300

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,97 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { AuthProvider } from '../contexts/AuthContext';
+'use client'
 
+import React from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Inter } from 'next/font/google';
+import { AuthProvider } from '@/contexts/AuthContext';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Reserva de Habitaciones',
-  description: 'Sistema de reserva de habitaciones',
-};
+const { Header, Content, Footer, Sider } = Layout;
 
-interface RootLayoutProps {
+const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: Array.from({ length: 4 }).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
+);
+
+interface AppProps{
   children: React.ReactNode;
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<RootLayoutProps>) {
+const App = ({children} : AppProps ) => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  
   return (
-    <html lang="es">
+     <html lang="es">
       <body className={inter.className}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <Layout>
+          <Header style={{ display: 'flex', alignItems: 'center' }}>
+            <div className="demo-logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              items={items1}
+              style={{ flex: 1, minWidth: 0 }}
+            />
+          </Header>
+          <div style={{ padding: '0 48px' }}>
+            <Breadcrumb
+              style={{ margin: '16px 0', background: colorBgContainer }}
+              items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
+            />
+            <Layout
+              style={{ padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG }}
+            >
+              <Sider style={{ background: colorBgContainer }} width={200}>
+                <Menu
+                  mode="inline"
+                  theme='dark'
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  style={{ height: '100%' }}
+                  items={items2}
+                />
+              </Sider>
+              <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                <AuthProvider>
+                  <AntdRegistry>
+                    {children}
+                  </AntdRegistry>
+                </AuthProvider>
+              </Content>
+            </Layout>
+          </div>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
       </body>
     </html>
   );
-}
+};
+
+export default App;
